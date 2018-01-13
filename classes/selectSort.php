c185ee0226eeec763be697ec4e44ccd9415c0a72
Unknown <nico.joachim@free.fr>
2018-01-13 15:40:32 +0100

    Sort by select algo

--- classes/selectSort.php
+++ classes/selectSort.php
@@ -10,16 +10,37 @@ class selectSort
 	function __construct($listStr)
 	{
 		$this->listStr = $listStr;
+		$this->stats["nb_it"] = 0;
+		$this->stats["time"] = 0;
 	}
 
 	public function toString()
 	{
-		echo "Tri par selection : ".$this->listStr;
+		$time_start = microtime();
+		$tabList = explode(",", $this->listStr);
+		$tabSize = count($tabList);
+		for ($i=0; $i < $tabSize; $i++) { 
+			$min = $i;
+			for ($j=$i+1; $j < $tabSize; $j++) { 
+				if ($tabList[$j] < $tabList[$min]) {
+					$min = $j;
+				}
+				$this->stats["nb_it"]++;
+			}
+			if ($min != $i) {
+				$tmp = $tabList[$i];
+				$tabList[$i] = $tabList[$min];
+				$tabList[$min] = $tmp;
+			}
+			$this->stats["nb_it"]++;
+		}
+		$time_end = microtime();
+		$this->stats["time"] = $time_end - $time_start;
+		echo "Tri par selection : ".implode(",", $tabList);
 	}
 
 	public function getStatsPerf()
 	{
-		$this->stats = array('nb_it' => "N/A", 'time' => "N/A");
 		return $this->stats;
 	}
 }
