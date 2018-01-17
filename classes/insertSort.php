65522f26bf5d2d61dfa461c834b51b15b4904054
Vubenoit <vu_b@etna-alternance.net>
2018-01-17 10:57:08 +0100

    insert sort

--- classes/insertSort.php
+++ classes/insertSort.php
@@ -1,27 +1,47 @@
 <?php
-/**
-* 
-*/
+
 class insertSort
 {
-	
-	private $listStr;
-	private $stats;
+    private $listStr;
+    private $stats;
 
-	function __construct($listStr)
-	{
-		$this->listStr = $listStr;
-	}
+    function __construct($listStr)
+    {
+        $this->listStr = $listStr;
+        $this->stats["nb_it"] = 0;
+        $this->stats["time"] = 0;
+    }
 
-	public function toString()
-	{
-		echo "Tri par insertion : ".$this->listStr;
-	}
+    public function getStatsPerf()
+    {
+        return $this->stats;
+    }
 
-	public function getStatsPerf()
-	{
-		$this->stats = array('nb_it' => "N/A", 'time' => "N/A");
-		return $this->stats;
-	}
+    function Tri_insrt()
+    {
+        $time_start = microtime();
+        $liste = explode(",", $this->listStr);
+        $taille = count($liste);
+        for($i = 0; $i < $taille; $i++)
+        {
+            $element_a_inserer = $liste[$i];
+            for($j = 0; $j < $i; $j++)
+            {
+                $element_courant = $liste[$j];
+                if ($element_a_inserer < $element_courant)
+                {
+                    $liste[$j] = $element_a_inserer;
+                    $element_a_inserer = $element_courant;
+                }
+                $this->stats["nb_it"]++;
+            }
+            $liste[$i] = $element_a_inserer;
+            $this->stats["nb_it"]++;
+        }
+        $time_end = microtime();
+        $this->stats["time"] = round(($time_end - $time_start)/1000, 8);
+        echo "Tri par insertion : ".implode(",", $liste);
+    }
 }
+
 ?>
