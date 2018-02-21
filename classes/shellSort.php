65f091e647939699e02375d2f050232f44f667cd
Unknown <nico.joachim@free.fr>
2018-02-21 11:35:26 +0100

    ShellSort

---
+++ classes/shellSort.php
@@ -0,0 +1,42 @@
+<?php
+require_once ('Sort.php');
+/**
+* 
+*/
+class shellSort extends Sort
+{
+
+	public function getSortedList()
+	{
+		$time_start = microtime(true);
+		$tabList = explode(",", $this->listStr);
+
+		$Tableau = explode(",", $this->listStr);
+
+		$Inversion = 0;
+		$ecart = count($Tableau);
+		do {
+			$ecart /= 2;
+			$this->stats["nb_it"]++;
+			do {
+				$Inversion = 0;
+				$this->stats["nb_it"]++;
+				for($I = 0; $I <= count($Tableau) - $ecart - 1; $I++) {
+					$this->stats["nb_it"]++;
+					$J = $I + $ecart;
+					if($Tableau[$J] < $Tableau[$I]) {
+						$Temporaire = $Tableau[$I];
+						$Tableau[$I] = $Tableau[$J];
+						$Tableau[$J] = $Temporaire;
+						$Inversion = 1;
+					}
+				}
+			} while (1 == $Inversion);
+		} while (1 != $ecart);
+
+		$time_end = microtime(true);
+		$this->stats["time"] = round(($time_end - $time_start), 8);
+		return $Tableau;
+	}
+}
+?>
---
+++ classes/templateSort.php
@@ -0,0 +1,20 @@
+<?php
+require_once ('Sort.php');
+/**
+* 
+*/
+class SORTNAMESort extends Sort
+{
+
+	public function getSortedList()
+	{
+		$time_start = microtime(true);
+		$NAMEOFTAB = explode(",", $this->listStr);
+
+		// PUT CODE
+
+		$time_end = microtime(true);
+		$this->stats["time"] = round(($time_end - $time_start), 8);
+		return $Tableau;
+	}
+}
