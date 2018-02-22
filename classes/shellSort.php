61a6d4b9218e019844cf6eb5c7dc724b1a80a2b1
Vubenoit <vu_b@etna-alternance.net>
2018-02-22 16:59:01 +0100

    2: Auto stash before merge of "algov2" and "origin/algov2"

--- classes/shellSort.php
+++ classes/shellSort.php
@@ -1,52 +1,45 @@
 <?php
 require_once ('Sort.php');
 /**
-* 
-*/
+ *
+ */
 class shellSort extends Sort
 {
-
-    public function insertSort($t, $gap, $debut)
+    function getSortedList()
     {
-        for ($i = $gap + $debut; $i < 20; $i+=$gap) {
-            $current = $t[$i];
-            for ($j = $i; $j >= $gap && $t[$j - $gap] > $current; $j-=$gap) {
-                $t[$j] = $t[$j - $gap];
-            }
-            $t[$j] = $current;
-        }
-    }
-    
-    public function getSortedList() {
-    	$time_start = microtime(true);
-        $gaps = array(701,301,132,57,23,10,4,1);
-		$array = explode(",", $this->listStr);
+        $time_start = microtime(true);
+        $liste = explode(",", $this->listStr);
 
-        $length=count($array);
 
-        $lgap=count($gaps);
+        $liste = $this->shell_Sort($liste);
 
-        for ($z=0;$z<$lgap;$z++) {
-            $this->stats["nb_it"]++;
-            $gap=$gaps[$z];
-            for ($i=$gap;$i<$length;$i++) {
-                $this->stats["nb_it"]++;
-                $element=$array[$i];
-                $j=$i;
-                while($j>=$gap && $array[$j-$gap]>$element) {
-                    $this->stats["nb_it"]++;
-                    //move value to right and key to previous smaller index
-                    $array[$j]=$array[$j-$gap];
-                    $j=$j-$gap;
-                    }
-                //put the element at index $j
-                $array[$j]=$element;
-            }
-        }
 
         $time_end = microtime(true);
-		$this->stats["time"] = round(($time_end - $time_start), 8);
-		return $array;
+        $this->stats["time"] = round($time_end - $time_start, 8);
+        return $liste;
     }
+    function shell_Sort($my_array)
+    {
+        $x = round(count($my_array)/2);
+        while($x > 0)
+        {
+            $this->stats["nb_it"]++;
+            for($i = $x; $i < count($my_array);$i++){
+                $temp = $my_array[$i];
+                $j = $i;
+                $this->stats["nb_it"]++;
+                while($j >= $x && $my_array[$j-$x] > $temp)
+                {
+                    $my_array[$j] = $my_array[$j - $x];
+                    $j -= $x;
+                    $this->stats["nb_it"]++;
+                }
+                $my_array[$j] = $temp;
+            }
+            $x = round($x/2.2);
+        }
+        return $my_array;
+    }
+
 }
 ?>
