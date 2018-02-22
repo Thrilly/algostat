06af1e7a5571e329878fec668b8f9565c14cb200
Vubenoit <vu_b@etna-alternance.net>
2018-02-22 16:59:01 +0100

    2: Auto stash before merge of "algov2" and "origin/algov2"

--- classes/quickSort.php
+++ classes/quickSort.php
@@ -1,24 +1,33 @@
 <?php
 require_once ('Sort.php');
-/**
- *
- */
+
 class quickSort extends Sort
 {
-    public function getSortedList($array = null)
+    function getSortedList()
     {
-        if ($array == null){
-            $array = explode(",", $this->listStr);
-        }
-        
         $time_start = microtime(true);
+        $liste = explode(",", $this->listStr);
+
+
+        $liste = $this->quick_sort($liste);
+
+
+        $time_end = microtime(true);
+        $this->stats["time"] = round($time_end - $time_start, 8);
+        return $liste;
+    }
+
+    function quick_sort($array)
+    {
         // find array size
         $length = count($array);
 
         // base case test, if array of length 0 then just return array to caller
         if($length <= 1){
             return $array;
-        } else{
+        }
+        else{
+
             // select an item to act as our pivot point, since list is unsorted first position is easiest
             $pivot = $array[0];
 
@@ -26,18 +35,19 @@ class quickSort extends Sort
             $left = $right = array();
 
             // loop and compare each item in the array to the pivot value, place item in appropriate partition
-            for ($i = 1; $i < count($array); $i++) {
-                if ($array[$i] < $pivot) {
+            for($i = 1; $i < count($array); $i++)
+            {
+                if((int)$array[$i] < (int)$pivot){
                     $left[] = $array[$i];
-                } else {
+                }
+                else{
                     $right[] = $array[$i];
                 }
-
+                $this->stats["nb_it"]++;
             }
-            $time_end = microtime(true);
-            $this->stats["time"] = round(($time_end - $time_start), 8);
-            return array_merge($this->getSortedList($left), array($pivot), $this->getSortedList($right));
-//            var_dump($array);
+
+            // use recursion to now sort the left and right lists
+            return array_merge($this->quick_sort($left), array($pivot), $this->quick_sort($right));
         }
     }
 }
